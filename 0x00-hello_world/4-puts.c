f9e9a25db7cbf0b69a912d32c4bddd9542e5eaa4
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-06-15 17:45:42 -0700

    All TASKS

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints exactly
+ * "Programming is like building a multilingual puzzle
+ *
+ * Return: Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: a C program that prints the size of various ty
+ * pes on the computer it is compiled and run on.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
+	return (0);
+}
