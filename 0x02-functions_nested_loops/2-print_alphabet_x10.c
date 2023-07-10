3bf1da1718b31a5a3c7d3c7e782e672d386a4d8b
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-07-10 08:32:08 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *print_alphabet - Prints the lowercase alphabet
+ *
+ *Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 97; i <= 122; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-
+void print_alphabet_x10(void);
 int _putchar(char c);
 void print_alphabet(void);
 
