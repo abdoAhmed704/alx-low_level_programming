8a81b8eac543f139318b317f8d2932295e3a7fcb
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-07-13 03:50:04 -0700

    sfg

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,28 +1,23 @@
 #include "main.h"
-
 /**
- * print_square - function that prints a square, followed by a new line.
- *
- * @size: is the size of the square
-*/
-
+ * print_square - draws a square line on the terminal.
+ * @size : number of to be printed
+ * Return:void
+ */
 void print_square(int size)
 {
-	int i, j;
+	int i = 0;
+	int j = 0;
 
 	if (size > 0)
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
-			{
+			for (j = 1; j <= size; j++)
 				_putchar('#');
-			}
 			_putchar('\n');
 		}
 	}
 	else
-	{
 		_putchar('\n');
-	}
 }
