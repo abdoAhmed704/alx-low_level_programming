b10e916eff9105ca6cd333986edd5f2a70cb0afb
abdoAhmed704 <132397042+abdoAhmed704@users.noreply.github.com>
2023-07-18 00:11:18 +0300

    all tasks
    all taslks

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a program that print name of of this source file
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _SUM_H
+#define _SUM_H
+
+#define SUM(a, b) ((a) + (b))
+
+#endif /* _SUM_H */
