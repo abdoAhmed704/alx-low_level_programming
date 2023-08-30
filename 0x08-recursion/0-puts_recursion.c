7987a3c56371b99e9aae3b380bef108aaffaa2c9
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-08-30 09:36:02 -0700

    Task_1

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_puts_recursion - a function that prints a string,
+ *followed by a new line.
+ *
+ *@s : a string
+ *
+ **/
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
