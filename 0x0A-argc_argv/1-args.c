e2442a912d932cd76d62bf24f83e864af5c5cb1a
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-01 08:19:41 -0700

    Task_0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: Number of Arguments
+ * @argv: Elements of Arguments
+ *
+ * Return: 0
+**/
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%i\n", argc);
+
+	return (0);
+}
