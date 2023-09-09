1dd3facf5f145b2116f24181adb6ee67c50a31c8
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-09 05:15:22 -0700

    Task_2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,44 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 
 /**
  * str_concat - Concatenates two strings.
  * @s1: The first string.
  * @s2: The second string.
  *
- * Return: A pointer to the concatenated string, or NULL if allocation fails.
+ * Return: A pointer to the concatenated string.
  */
 char *str_concat(char *s1, char *s2)
 {
-	long len_1 = 0;
-	long len_2 = 0;
+	int len1 = 0, len2 = 0, index1 = 0, index2 = 0;
 	char *ptr;
-	long index_1 = 0;
-	long index_2 = 0;
 
-	if (s2 == NULL)
-		s2 = "\0";
 	if (s1 == NULL)
-		s1 = "\0";
-		/** Allocate the pointer **/
-	ptr = (char *)malloc((len_1 + len_2 + 1) * sizeof(char));
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
+
+	/* Calculate the length of the strings */
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+
+	ptr = (char *)malloc((len2 + len1) * sizeof(char) + 1);
 
 	if (ptr == NULL)
-	{
 		return (NULL);
-	}
 
-	for (index_1 = 0; index_1 < len_1; index_1++)
-	{
-		ptr[index_1] = s1[index_1];
-	}
+	/* Copy the characters from s1 to ptr */
+	for (index1 = 0; index1 < len1; index1++)
+		ptr[index1] = s1[index1];
 
-	for (index_2 = 0; index_2 < len_2; index_2++)
-	{
-		ptr[index_1 + index_2] = s2[index_2];
-	}
-	ptr[index_1 + index_2] = '\0';
+	/* Copy the characters from s2 to ptr */
+	for (index2 = 0; index2 < len2; index2++)
+		ptr[index2 + len1] = s2[index2];
+
+	ptr[len1 + len2] = '\0';
 
 	return (ptr);
 }
+
