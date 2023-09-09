380aeaa8b402b12274fbba6f430969099090c7c3
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-09 05:17:03 -0700

    Task_2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,9 @@ char *str_concat(char *s1, char *s2)
 	char *ptr;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "\0";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
 	/* Calculate the length of the strings */
 	while (s1[len1])
