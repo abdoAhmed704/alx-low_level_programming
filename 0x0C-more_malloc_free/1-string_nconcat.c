0c35ca6c766f2ce415042d462ceae7be8c935d07
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-11 07:37:57 -0700

    Task_1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -34,11 +34,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 	{
-		s1 = '\0';
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s2 = '\0';
+		s2 = "";
 	}
 
 	len1 = Get_length(s1);
