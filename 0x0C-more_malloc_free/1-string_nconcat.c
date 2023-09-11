1600dfc7c9171e488f498c71766cb84d22876a92
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-11 07:41:55 -0700

    Task_1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -58,7 +58,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; i < n; i++)
 		ptr[i + len1] = s2[i];
 
-	ptr[len1 + n + 1] = '\0';
+	ptr[len1 + n] = '\0';
 
 	return (ptr);
 }
