9f83b8fdc0572e1f3aac82762686b0b99cb8e7bc
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-11 07:25:37 -0700

    Task_1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,62 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * string_nconcat - a function that concatenates two strings.
+ * Get_length - get the length of str
  *
- * @n: Number of bytes.
- * @s1: a pointer to first string.
- * @s2: a pointer to 2nd string.
+ * @s: a string
  *
- * Return: Pointer.
- */
+ * Return: length
+**/
+int Get_length(char *s)
+{
+	int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
+ * string_nconcat - concatenates two strings.
+ *
+ * @s1: a string
+ * @s2: a string
+ * @n: number tjat conc
+ *
+ * Return: a pointer to the new string
+**/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int k, u, s1_length, s2_length;
+	int len1, len2, i;
 
-	if (s2 == NULL)
-		s2 = "";
 	if (s1 == NULL)
-		s1 = "";
-	for (s2_length = 0; s2[s2_length] != '\0'; s2_length++)
-	;
-	for (s1_length = 0; s1[s1_length] != '\0'; s1_length++)
-	;
-	str = malloc(s1_length + n + 1);
-	if (str == NULL)
 	{
+		s1 = "\0";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "\0";
+	}
+
+	len1 = Get_length(s1);
+	len2 = Get_length(s2);
+
+	if (n > len2)
+		n = len2;
+
+	char *ptr = malloc((len1 + n + 1) * sizeof(char));
+
+	if (ptr == NULL)
 		return (NULL);
-	}
-	for (k = 0; s1[k] != '\0'; k++)
-		str[k] = s1[k];
-	for (u = 0; u < n; u++)
-	{
-		str[k] = s2[u];
-		k++;
-	}
-	str[k] = '\0';
-	return (str);
+
+	for (i = 0; i < len1; i++)
+		ptr[i] = s1[i];
+
+	for (i = 0; i < n; i++)
+		ptr[i + len1] = s2[i];
+
+	ptr[len1 + n + 1] = '/0';
+
+	return (ptr);
 }
