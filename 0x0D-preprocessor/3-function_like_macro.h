e5a68a3d350a35d6f1c14a7191683d30a5bbb0e7
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-12 14:16:36 -0700

    Task_3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef _FUNCTION_LIKE_MACRO_H
 #define _FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (((-x) > (x)) ? (-x) : (x))
+#define ABS(x) (((-(x)) > (x)) ? (-(x)) : (x))
 
 #endif /* _FUNCTION_LIKE_MACRO_H */
 
