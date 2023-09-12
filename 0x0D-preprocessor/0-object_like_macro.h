b104a2d46793ecd28af4bd1c83e1d03b1c55737d
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-12 13:14:55 -0700

    Task_0

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,6 +1,6 @@
-#ifndef _OBJECT_LIKE_MACRO_H
-#define _OBJECT_LIKE_MACRO_H
+#ifndef _0-OBJECT_LIKE_MACRO_H_
+#define _0-OBJECT_LIKE_MACRO_H_
 
 #define SIZE 1024
 
-#endif /* _OBJECT_LIKE_MACRO_H */
+#endif 
