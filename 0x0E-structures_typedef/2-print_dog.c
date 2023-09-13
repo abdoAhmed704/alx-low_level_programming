5d5acb1b045d40ac73e1b8f3c5c7f0e7f03e0d85
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-13 13:54:31 -0700

    Task_4

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,24 +1,18 @@
-#include "main.h"
+#include "dog.h"
+#include <stdio.h>
+
 /**
- * print_dog - print data
+ * print_dog - prints a dog
+ * @d: the dog to print
  *
- * @d: pointer to an object
- *
- * Return: (void).
+ * Return: void.
  */
 void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name)
-			printf("Name: %s\n", d->name);
-		else
-			printf("Name: (nil)\n");
+		printf("Name: %s\n", d->name ? d->name : "(nil)");
 		printf("Age: %f\n", d->age);
-
-		if (d->owner)
-			printf("Owner: %s\n", d->owner);
-		else
-			printf("Owner: (nil)\n");
+		printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 	}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ *
+ * Description: just a lone dog struct in a big kitty world
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+}dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
