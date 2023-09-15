9069da96e7a951138b79eb3e59e3cc81fb85b543
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-15 06:03:48 -0700

    Task_1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,6 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_list args;
 
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
