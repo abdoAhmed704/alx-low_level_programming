d47615e6a1d1d5120671b73a695cad8e44d50a0e
abdoAhmed704 <abdoahmedsayed704@gmail.com>
2023-09-26 10:53:50 -0700

    Edit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *nod;
 	unsigned int n;
 
-	for (nod = head, n = 0; nod && n < index; node = nod->next, n++)
+	for (nod = head, n = 0; nod && n < index; nod = nod->next, n++)
 		;
 	return (nod);
 }
