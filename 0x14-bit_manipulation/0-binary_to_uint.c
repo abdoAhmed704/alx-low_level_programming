3254a504cf5f1dfbb54efd1d8b3e7659d5c16790
abdoAhmed704 <132397042+abdoAhmed704@users.noreply.github.com>
2023-09-29 18:49:17 +0300

    Update 0-binary_to_uint.c
    z

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,22 +1,10 @@
-#include "main.h"
-
-/**
- * binary_to_uint - converts a binary
- * @b: a string
- *
- * Return: the unsigned
- */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum = 0;
-
-	if (!b)
-		return (0);
-	while (*b)
-	{
-		if (*b != '0' && *b != '1')
-			return (0);
-		sum = sum * 2 + (*b++ - '0');
-	}
-	return (sum);
+    unsigned int i = 0;
+    while (*b)
+    {
+        i = (i << 1) + (*b == '1');
+        b++;
+    }
+    return i;
 }
