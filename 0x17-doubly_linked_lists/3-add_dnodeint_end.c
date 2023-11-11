a2f2e76956249c77d3a6db57baf0cfced4d50f3e
= <abdoahmedsayed704@gmail.com>
2023-11-11 09:35:51 -0800

    (╥﹏╥)

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,15 +1,17 @@
 #include "lists.h"
 /**
- * add_dnodeint - add a node at the beginning
+ * add_dnodeint_end - add a node at the end 
  *
  * @head: pointer to the first node
  * @n: the data
  *
- * Return: the length of linked list
+ * Return: pointer to the added node
 */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *tempNode = NULL;
+	dlistint_t *ptr_head = *head;
+
 
 	tempNode = malloc(sizeof(dlistint_t));
 	if (tempNode != NULL)
@@ -23,14 +25,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		}
 		else
 		{
-			tempNode->next = *head;
-			tempNode->prev = NULL;
-			(*head)->prev = tempNode;
-			*head = tempNode;
+			while ((*head)->next)
+			{
+				*head = (*head)->next;
+			}
+			tempNode->next = NULL;
+			tempNode->prev = *head;
+			(*head)->next = tempNode;
+			*head = ptr_head;
+
 		}
 	}
 	else
+	{
 		return (NULL);
+	}
 	return (tempNode);
-
 }
