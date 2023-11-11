fb63820bc6619e423f3cc4118e940faabe3d73dd
= <abdoahmedsayed704@gmail.com>
2023-11-11 09:22:10 -0800

    (╥﹏╥)

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * 
+ * dlistint_len - return the length of linked list
+ *
+ * @h: pointer to the first node
+ *
+ * Return: the length of linked list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t count = 0;
+    while (h)
+    {
+        count++;
+        h = h->next;
+    }
+    return (count);
+}
