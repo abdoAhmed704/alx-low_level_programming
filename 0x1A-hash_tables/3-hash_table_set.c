ad5c8d2f36877d84876745f79d3e0fbe0091fd34
= <abdoahmedsayed704@gmail.com>
2023-12-02 20:08:05 +0200

    😊

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *current;
-	hash_node_t *new_nod;
+	hash_node_t *new_node;
 	unsigned long int index;
 
 	if (!ht || !key)
@@ -32,7 +32,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		current = current->next;
 	}
-	new_node = malloc(sizeof(hash_node_t *))
+	new_node = malloc(sizeof(hash_node_t *));
 	if (!new_node)
 		return (0);
 	new_node->key = strdup(key);
