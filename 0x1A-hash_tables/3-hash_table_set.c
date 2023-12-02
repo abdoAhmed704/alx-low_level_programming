7f78e93d3963d6ab92fad852065563be48208f67
= <abdoahmedsayed704@gmail.com>
2023-12-02 20:34:03 +0200

    😊

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_set - setting the key and value
  *
@@ -15,8 +16,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index;
 
-	if (!ht || !key || !value || !ht->array ||
-		ht->size == 0 || strlen(key) == 0)
+	if (ht == NULL || key == NULL || value == NULL ||
+		ht->array == NULL || ht->size == 0 || strlen(key) == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
