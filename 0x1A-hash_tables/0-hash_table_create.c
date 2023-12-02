9910fef80081bbb670d7b6abe8fc8aeb968dc6d3
= <abdoahmedsayed704@gmail.com>
2023-12-02 15:16:26 +0200

    😊

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,14 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int idx;
 	hash_table_t *new_table;
 
-	if (!size)
-		return (NULL);
 	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
+	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t) * size);
-	if (!new_table->array)
+	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
