bff368b2a841ea095dc65a175ffe03e5cba661ee
= <abdoahmedsayed704@gmail.com>
2023-12-02 19:17:49 +0200

    😊

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,30 +1,27 @@
 #include "hash_tables.h"
-
 /**
- * hash_table_create - creates a hash table with a given size
+ * hash_table_create - creat a Hash Table
  *
- * @size: size of the hash table
- * Return: the created hash table, or NULL if function fails
- */
+ * @size: the size of the table
+ *
+ * Return: new table
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	hash_node_t **array;
-	unsigned long int i;
+	unsigned long int idx;
+	hash_table_t *new_table;
 
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
 		return (NULL);
-
-	array = malloc(sizeof(hash_node_t *) * size);
-	if (array == NULL)
+	new_table->size = size;
+	new_table->array = malloc(size * sizeof(hash_node_t *));
+	if (new_table->array == NULL)
+	{
+		free(new_table);
 		return (NULL);
-
-	for (i = 0; i < size; i++)
-		array[i] = NULL;
-
-	table->array = array;
-	table->size = size;
-
-	return (table);
+	}
+	for (idx = 0; idx < size; idx++)
+		new_table->array[idx] = NULL;
+	return (new_table);
 }
