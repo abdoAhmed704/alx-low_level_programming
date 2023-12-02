9f38f7237a170d09a324e90640ad1180def72d22
= <abdoahmedsayed704@gmail.com>
2023-12-02 20:38:57 +0200

    😊

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,11 +1,9 @@
-#ifndef _HASH_TABLES_H
-#define _HASH_TABLES_H
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
 
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-
-
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -36,10 +34,6 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-
-
-
-
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -47,11 +41,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
-unsigned long int hash_djb2(const unsigned char *str);
 
-
-
-
-
-
-#endif /*_HASH_TABLES_H*/
+#endif /* HASH_TABLES_H */
