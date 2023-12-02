84a539b6b314fd241a45d0d637fc218e029ae0e5
= <abdoahmedsayed704@gmail.com>
2023-12-02 20:09:02 +0200

    😊

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index;
 
-	if (!ht || !key)
+	if (!ht || !key || !value)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
