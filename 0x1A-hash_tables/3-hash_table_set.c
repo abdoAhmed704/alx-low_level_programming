e21bb8b35273de38a615378d2ef81f4ad3e428e7
= <abdoahmedsayed704@gmail.com>
2023-12-02 20:35:45 +0200

    😊

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,23 +27,23 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(current->key);
 			current->key = strdup(key);
-			if (!current->value)
+			if (current->value == NULL)
 				return (0);
 			return (1);
 		}
 		current = current->next;
 	}
 	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
+	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
-	if (!new_node->key)
+	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
 	new_node->value = strdup(value);
-	if (!new_node->value)
+	if (new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
