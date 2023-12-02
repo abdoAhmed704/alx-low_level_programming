9affac2e9d4ae5239e9ec2a39a70eeb6a4df0b0d
= <abdoahmedsayed704@gmail.com>
2023-12-02 15:10:25 +0200

    😊

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creat a Hash Table
+ *
+ * @size: the size of the table
+ *
+ * Return: new table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int idx;
+	hash_table_t *new_table;
+
+	if (!size)
+		return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table)
+		return (NULL);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+	if (!new_table->array)
+	{
+		free(new_table);
+		return (NULL);
+	}
+	for (idx = 0; idx < size; idx++)
+		new_table->array[idx] = NULL;
+	return (new_table);
+}
