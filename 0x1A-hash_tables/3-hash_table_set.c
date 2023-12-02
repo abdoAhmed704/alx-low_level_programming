f05abd20adf6409284e3334bbbd9ea683110a9e0
= <abdoahmedsayed704@gmail.com>
2023-12-02 20:18:24 +0200

    😊

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	unsigned long int index;
 
-	if (!ht || !key || !value || !key->array
+	if (!ht || !key || !value || !key->array ||
 		ht->size == 0 || strlen(key) == 0)
 		return (0);
 	index = key_index((unsigned char *)key, ht->size);
