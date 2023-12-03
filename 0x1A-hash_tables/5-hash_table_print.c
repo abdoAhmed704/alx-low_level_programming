eb619ebf77feeb2181a27bd9e1681c88969ce06e
= <abdoahmedsayed704@gmail.com>
2023-12-03 18:38:02 +0200

    😊

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - Get the value
+ *
+ * @ht: the hash table
+ *
+ * Return: (void)
+ *
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *current;
+
+	printf("{");
+	for (index = 0; index < ht->size; index++)
+	{
+		current = ht->array[index];
+		while (current)
+		{
+			if (current->key != NULL)
+			{
+				printf("'%s': '%s'", current->key, current->value);
+			}
+			current = current->next;
+			if (current != NULL)
+				printf(", ");
+		}
+	}
+	printf("}\n");
+}
